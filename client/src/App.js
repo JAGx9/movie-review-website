c8a4bc8b83ca22c4529497e24dd9f005e50abded
JAGx9 <jagonzalezf9@gmail.com>
2022-08-13 13:43:53 -0700

    Created new repo - first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
